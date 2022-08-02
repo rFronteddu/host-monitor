dfde246ac048c00bb4a23a59d7913c6bc1fca2e1
Christianne Barry <cb218@students.uwf.edu>
2022-08-01 19:28:44 -0500

    moved reboot counter to testbed-monitor

--- sensors/host.go
+++ sensors/host.go
@@ -22,23 +22,15 @@ func NewHostSensor(period time.Duration) *Host {
 
 func (sensor *Host) Poll(measure *measure.Measure) {
 	time.Sleep(sensor.period)
-	// Reset the reboot counter every midnight
-	if time.Now().Hour() == 0 && time.Now().Minute() == 0 {
-		dailyRebootCounter = 0
-	}
-	// Get host info
+
 	h, _ := host.Info()
-	if lastBootTime < h.BootTime {
-		lastBootTime = h.BootTime
-		dailyRebootCounter++
-	}
-	// Print report and send to Measure channel
 	fmt.Printf("Host Report - Host ID: %v Host Name: %v, OS: %v, Platform: %v, Arch: %v, Boot Date: %v, Reboots Today: %v, Uptime: %v\n", h.HostID, h.Hostname, h.OS, h.Platform, h.KernelArch, time.Unix(int64(h.BootTime), 0), dailyRebootCounter, h.Uptime)
+
 	measure.Strings["host_id"] = h.HostID
 	measure.Strings["host_name"] = h.Hostname
 	measure.Strings["os"] = h.OS
 	measure.Strings["platform"] = h.Platform
 	measure.Strings["kernelArch"] = h.KernelArch
 	measure.Strings["bootTime"] = time.Unix(int64(h.BootTime), 0).Format(time.RFC822)
-	measure.Integers["Reboots_Today"] = dailyRebootCounter
+	measure.Integers["uptime"] = int64(h.Uptime)
 }
