8c147deeb6cf149cd1cb73e8e3c41ff2ab1bded0
cb218 <cb218@students.uwf.edu>
2022-10-23 15:21:46 -0500

    fix date in reboots.sh

--- probers/reboots.sh
+++ probers/reboots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #This script used only by linux/darwin
-day=$(date | awk {'print $1 " " $3 " " $2'})
+day=$(date | awk {'print $1 " " $2 " " $3'})
 reboot_times=$(last reboot | grep "$day" | wc -l)
-echo "$reboot_times"
+echo $reboot_times
